04058e1495ea52b35cbd8b0737704e578ebe5f76
Pavel <triodjangopiter@yandex.ru>
2017-10-29 17:11:25 +0300

    auth

--- src/main/java/ru/javavision/servlets/servlet/LoginServlet.java
+++ src/main/java/ru/javavision/servlets/servlet/LoginServlet.java
@@ -3,12 +3,8 @@ package ru.javavision.servlets.servlet;
 import javax.servlet.http.HttpServlet;
 
 public class LoginServlet extends HttpServlet {
-
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-//            throws ServletException, IOException {
-//
-//        req.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(req, resp);
-//    }
-
-
+    /*
+      All login logic:
+      @see ru.javavision.servlets.filter.AuthFilter;
+     */
 }
