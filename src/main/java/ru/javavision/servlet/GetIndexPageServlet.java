0ab7474b269a09655ad2b39a7a603cfb8f04f45d
Pavel <triodjangopiter@yandex.ru>
2017-10-30 22:20:39 +0300

    complete auth sys

--- src/main/java/ru/javavision/servlet/GetIndexPageServlet.java
+++ src/main/java/ru/javavision/servlet/GetIndexPageServlet.java
@@ -5,14 +5,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GetIndexPageServlet extends HttpServlet {
-
-    private static String index = "/WEB-INF/view/index.jsp";
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        req.getRequestDispatcher(index).forward(req, resp);
+
+        req.setAttribute("simpleOutput", new Poem());
+
+        final List<String> operations = new ArrayList<>();
+        operations.add("eq – проверка на равенство");
+        operations.add("ne – проверка на неравенство");
+        operations.add("lt – строго менее чем");
+        operations.add("gt – строго более чем");
+        operations.add("le – меньше либо равно чему-то");
+        operations.add("ge – больше или равно чему-то");
+        req.setAttribute("operations", operations);
+
+        req.setAttribute("condition", true);
+
+        req.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(req, resp);
+    }
+
+    public static class Poem {
+        private String text = "Я помню море пред грозою: \n" +
+                "Как я завидовал волнам, \n" +
+                "Бегущим бурной чередою \n" +
+                "С любовью лечь к ее ногам! \n" +
+                "А. С. Пушкин";
+
+        public String getText() {
+            return text;
+        }
     }
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -6,12 +6,40 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
-
+    <title>Подробный разбор возможностей библиотеки</title>
+    <%--Справка : http://ru.bmstu.wiki/JSTL_(JavaServer_Pages_Standard_Tag_Library)--%>
 </head>
 <body>
 
-    Hello from Java Vision!
+
+<a target="_blank"
+   href="http://ru.bmstu.wiki/JSTL_(JavaServer_Pages_Standard_Tag_Library)">
+    Подробный разбор возможностей библиотеки jstl тут.
+</a>
+<hr />
+
+
+<h2>Пример использования вывода на данных</h2>
+<div>При использовании "out" контекст указывается в формате requestScope.youreObj"</div>
+<c:out value="${requestScope.simpleOutput.text}"/>
+<hr />
+
+
+<div>При использовании "set" контекст указывается в формате scope="request"</div>
+<c:set var="operations" scope="request" value="${operations}" />
+<hr />
+
+<h2>Пример использования цикла:</h2>
+<c:forEach items="${operations}" var="operation">
+    <c:out value="${operation}"/><br />
+</c:forEach>
+<hr />
+
+
+<h2>Пример использования условного оператора:</h2>
+<c:if test="${requestScope.condition eq true}">
+    <div>Сработал условный оператор</div>
+</c:if>
 
 </body>
 </html>
